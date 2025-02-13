b889146e13cf0fcd0cd905ca870b52b3cccf0000
Bharat <bharathariag15@gmail.com>
2025-02-13 15:53:08 +0530

    Added the CRUD functionalities in this test DRF app

--- home/views.py
+++ home/views.py
@@ -19,5 +19,32 @@ def personView(request):
     if request.method == 'GET':
         person = Person.objects.all()
         serializer = PersonSerializer(person, many=True)
-
-    
+        return Response(serializer.data)
+    if request.method == 'POST':
+        data = request.data
+        serializer = PersonSerializer(data = data, many= True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    if request.method == 'PUT':
+        data = request.data
+        person = Person.objects.get(id = data['id'])
+        serializer = PersonSerializer(person, data, partial = False)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    if request.method == 'PATCH':
+        data = request.data
+        person = Person.objects.get(id = data['id']) 
+        serializer = PersonSerializer(person, data, partial = True) #Only diff between PUT and PATCH is Partial
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    else:
+        data = request.data
+        person = Person.objects.get(id=data['id'])
+        person.delete()
+        return Response(f"Record with id:{data['id']} deleted successfully" )
